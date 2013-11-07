319308dafdfee37ccd5d1ffae69967e7888c16a2
m1almatal <m1almatal@gmail.com>
2013-11-07 21:10:25 +0100

    Implementation of OWL converter. Still error Exception in thread "main" java.lang.NoClassDefFoundError: aterm/ATerm 	at openDataWrapper.OWLConverter.run(OWLConverter.java:67) 	at openDataWrapper.Principale.owl(Principale.java:158) 	at openDataWrapper.Principale.main(Principale.java:136) Caused by: java.lang.ClassNotFoundException: aterm.ATerm 	at java.net.URLClassLoader$1.run(URLClassLoader.java:366) 	at java.net.URLClassLoader$1.run(URLClassLoader.java:355) 	at java.security.AccessController.doPrivileged(Native Method) 	at java.net.URLClassLoader.findClass(URLClassLoader.java:354) 	at java.lang.ClassLoader.loadClass(ClassLoader.java:424) 	at sun.misc.Launcher$AppClassLoader.loadClass(Launcher.java:308) 	at java.lang.ClassLoader.loadClass(ClassLoader.java:357) 	... 3 more

--- src/main/java/openDataWrapper/OWLConverter.java
+++ src/main/java/openDataWrapper/OWLConverter.java
@@ -1,8 +1,13 @@
 package openDataWrapper;
 
+import java.io.BufferedWriter;
+import java.io.FileWriter;
+import java.io.IOException;
+
 import org.mindswap.pellet.jena.ModelExtractor;
 
 import com.hp.hpl.jena.rdf.model.Model;
+import com.hp.hpl.jena.util.FileManager;
 
 /**
  * 
@@ -53,8 +58,10 @@ public class OWLConverter {
 	 * Main method running OWLConverter on chosen datasources
 	 */
 	public void run() {
-		// TODO implement
-		System.err.println("Not Implemented yet");
+
+		StringBuilder outputFile = new StringBuilder("src/main/resources/output/owl/");
+		
+		loadLinkedDataset(outputFile);
 
 		// Create an inference extractor
 		ModelExtractor extractor = new ModelExtractor(masterModel);
@@ -62,7 +69,63 @@ public class OWLConverter {
 		// Extract default set of inferences
 		Model inferences = extractor.extractModel();
 
-		// inferences.write()
-	}
+		// write loaded datasets into output files
+		writeModelIntoOutputFile(inferences, outputFile);
 
+		// empty model
+		emptyModel();
+		
+	}
+	
+	/**
+	 * Methode permettant de charger un dataset issu d'autres datasets
+	 */
+	private StringBuilder loadLinkedDataset(StringBuilder outputFile) {
+		String pathToRequestedLinkedDataset = DataSourceManager.chooseAvailableLinkedDatasets();
+		loadDataset(DataSourceManager.PATH_TO_LINKED_DATASETS.append("/").append(pathToRequestedLinkedDataset).toString());
+		return outputFile.append(pathToRequestedLinkedDataset);
+	}
+	
+	/**
+	 * Load the specified data source data into the jena model.
+	 * 
+	 * @param ttl
+	 *            the turtle input of data.
+	 */
+	private void loadDataset(String ttl) {
+		if (masterModel == null) {
+			masterModel = FileManager.get().loadModel("file:" + ttl, "N3");
+		} else {
+			try {
+				FileManager.get()
+						.readModel(masterModel, "file:" + ttl, "N3");
+			} catch (Exception e) {
+				System.err.println("The n3 file doesn't exist " + ttl + "  "
+						+ e.getMessage());
+			}
+		}
+	}
+	
+	
+	/**
+	 * Writes models into output files
+	 * 
+	 * @param outputFileN3
+	 *            output file for .n3 file
+	 * @param outputFileRDFXML
+	 *            output file for .rdf file
+	 */
+	private void writeModelIntoOutputFile(Model model, StringBuilder outputFile) {
+		BufferedWriter out;
+		try {
+			out = new BufferedWriter(new FileWriter(outputFile
+					.append(".owl").toString()));
+			model.write(out, "N3");
+			out.close();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+
+
+	}
 }
